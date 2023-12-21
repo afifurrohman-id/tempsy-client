ba66e88a03fe4f91e58308a40cde844bc9a1ef0c
afifurrohman-id <afifurrohman.id@gmail.com>
2023-12-21 07:51:58 +0700

    refactor: Remove unused favicon middleware, because already have favicon.

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/compress"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/template/html/v2"
@@ -35,7 +34,7 @@ func main() {
 
 	app.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed,
-	}), recover.New(), logger.New(), favicon.New(), middleware.Limiter)
+	}), recover.New(), logger.New(), middleware.Limiter)
 
 	cacheDuration := 10 * time.Second
 	if os.Getenv("APP_ENV") != "production" {
